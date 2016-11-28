546c02e790e3f514c4cefa8eb0a24f8011b9f731
Сергей Ермаков <podkot@gmail.com>
2016-11-28 05:08:55 +0300

    .sshrc

---
+++ bin/setup/centos.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#
+sudo yum install tig mc vim editorconfig
+
+# RHEL7+
+sudo yum install the_silver_searcher
---
+++ bin/setup/debian.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo apt-get install tig mc vim editorconfig
+
+# debian >= 8, ubuntu >= 13.10
+sudo apt-get install silversearcher-ag
