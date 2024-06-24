f165e37bc21ece40d838dbc60c5d02428a77b714
Сергей Ермаков <podkot@gmail.com>
2024-06-24 09:21:00 +0300

    var example up

--- bashrc_vars_example.bash
+++ bashrc_vars_example.bash
@@ -6,10 +6,10 @@ export SCM_CHECK_HG=false
 export PROMPT_LAST_COMMAND_TIME=true
 
 # versions
-export PHP_VERSION=8.0
+export PHP_VERSION=8.3
 export APACHE_VERSION=2.4
-export NODE_VERSION=16
-export PYTHON_VERSION=3.9
+export NODE_VERSION=22
+export PYTHON_VERSION=3.12
 
 # stuff
 export DIGNS_DEFAULT_NS=8.8.8.8
